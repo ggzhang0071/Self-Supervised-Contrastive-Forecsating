d148a4dd5a73045a9affab2cd672357a1b28ed26
zhanggg <zhanggg@zhaomei.com>
2024-08-08 11:43:09 +0800

    test MS and M feature

--- scripts/AutoCon_beigang.sh
+++ scripts/AutoCon_beigang.sh
@@ -8,6 +8,8 @@ feature="S"
 root_path="/git/datasets/beigang_data/${target}"
 data_path="runmin_an_factors_4500K1.0S.csv"
 
+for featue in  "S"  "MS"
+do 
 if [ "$feature" == "S" ]; then
     enc_in=1
 elif [ "$feature" == "MS" ]; then
@@ -30,3 +32,4 @@ nohup python -u run_optuna.py --AutoCon  --AutoCon_multiscales 336 --AutoCon_wno
 
 nohup python -u run_optuna.py --AutoCon  --AutoCon_multiscales 720 --AutoCon_wnorm Decomp  --AutoCon_lambda 0.01  --d_model 8 --d_ff 8 --e_layers 2 --target $target --c_out 1 --root_path $root_path  --data_path $data_path --model_id ICLR24_CRV --model AutoConNet --data beigang --seq_len 336 --label_len 48 --pred_len 10 --enc_in $enc_in --des 'Exp' --itr $nitr --batch_size 128 --learning_rate 0.0001 --lradj fixed --feature $featue  --target $target \
 --config param_config_long_term_forecast.json 
+done
